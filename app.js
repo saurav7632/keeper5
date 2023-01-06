5ea9cf41221462e294bcc9a36b9d23c3036b2a6b
saurav7632 <121542006+saurav7632@users.noreply.github.com>
2023-01-06 09:58:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -250,4 +250,4 @@ app.get("*",function (req,res) {
     res.sendFile(path.join(__dirname,"./client/build/index.html"));
   })
 
-app.listen(3000()=>{ console.log("Server started on port 8080")});
+app.listen(8080()=>{ console.log("Server started on port 8080")});
